0728598d2a22b9a4cf83eda59e50a24f348fc7f6
m-fiks <fiksd005@umn.edu>
2018-05-10 14:22:24 -0500

    survey added

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ app = express();
 
 const PORT = 8080;
 
+app.get('/', (req,res)=>{
+    res.send('hello');
+})
+
+app.get('/survey', (req, res) =>{
+    res.sendFile(path.join(__dirname, "/public/survey.html"))
+})
+
 app.listen(PORT, () =>{
     console.log(`server started at port: ${PORT}`)
 })
