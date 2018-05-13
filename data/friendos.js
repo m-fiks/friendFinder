6286eda6d5978f71a29a13d2bd5dff6053d455c5
m-fiks <fiksd005@umn.edu>
2018-05-13 00:25:59 -0500

    logic to compare responses

--- data/friendos.js
+++ data/friendos.js
@@ -5,51 +5,23 @@ const friendArray = [
         name:'Edward',
         photo: 'https://lh3.googleusercontent.com/AHBAqL9CLzvawav8ab03ojHZD9gGYndKB04J5Iqv-AFxD4g4UMY5Jh-6bDbyOzAaiTf3a9Z2epKKKiKPRKxkX3HaSKnUKFsm_N0uWUJN06JTKDMUEzvv3el5msGgD19KDP52AVV7rw=w2400',
         questions: [
-            '1',
-            '2',
-            '2',
-            '4',
-            '1',
-            '5',
-            '5',
-            '1',
-            '2',
-            '4'
-        ]
+            1,2,2,4,1,5,5,1,2,4],
     },
     {
         name:'Weird Seal',
         photo: 'https://lh3.googleusercontent.com/Z01yl1tLNdrw5cljuSlHwW3sSlX8pW9GI-t49rYUi9jg8U73IK7qnDmWY6yiAtLJ2MPsuWrgbibUFSvZDv8JTQBBsBhCFl-xGbkuR0ktnyjHnZ5WkYwpWQk9lfaocWUSoi1_rSqzyw=w2400',
         questions: [
-            '1',
-            '1',
-            '1',
-            '1',
-            '1',
-            '1',
-            '1',
-            '1',
-            '1',
-            '1'
-        ]
+            1,1,1,1,1,1,1,1,1,1],  
     },
     {
         name:'Cold Press',
         photo: 'https://lh3.googleusercontent.com/cw7R0mG4BEPpq-PFFPdtagX1xIrRXjNu3mo_11v-Ty8KbcIXY8BqUg0QYS_oj3pGHerxDGorkm-cTcfCrJRg4oCpN1pi2ir8r39wY3EN-xLmTloI1CH8GGxYLrsKeoF5n7u6rAAsQw=w2400',
         questions: [
-            '5',
-            '5',
-            '5',
-            '5',
-            '5',
-            '5',
-            '5',
-            '5',
-            '5',
-            '5'
-        ]
+            5,5,5,5,5,5,5,5,5,5],
     }
-
 ]
+
+
+
 
 module.exports = friendArray;
--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,22 +1,57 @@
 const path = require('path');
 const bodyParser = require('body-parser');
+const getClosest = require("get-closest");
 
-const array = require('../data/friendos.js');
-// console.log(array);
+let friendArray = require('../data/friendos.js');
 
 module.exports = (app) => {
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.get('/api/survey', (req,res) => {
-    res.send(array)
+app.get('/api/friends', (req,res) => {
+    res.send(friendArray)
 })
 
 //get survey data
 app.post('/api/survey', (req,res)=>{
-   let newFriend = req.body;
-   array.push(newFriend);
+   //get user input
+   let userInput = req.body;
+
+   //target questions and convert to integers
+   let userResp = userInput.questions;
+   //let newArray = newQuest.map(elem => parseInt(elem));
+
+   let bestMatch = {
+    matchName : "",
+    matchPhoto : "",
+    totalDifference : 100
+   }
+   
+
+   //iterate thru friends array
+   for (let i=0; i<friendArray.length; i++){
+       //console.log(friendArray[i])
+       let diff = 0;
+
+       for (let j = 0; j < userResp.length; j++){
+           //get absolute value of diff between each question compared to userresp
+           diff += Math.abs(friendArray[i].questions[j] - userResp[j] )
+       }
+
+       if (diff < bestMatch.totalDifference){
+           //set total diff and set matchname and photo to best match
+           bestMatch.totalDifference = diff;
+           bestMatch.matchName = friendArray[i].name;
+           bestMatch.matchPhoto = friendArray[i].photo;
+       }
+   }
+
+   friendArray.push(userResp);
+
+    //res.json(bestMatch)
+   console.log(bestMatch)
 })
 
 };
+
